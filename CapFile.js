f81eca7a610f4490f4b0c0f0cdaff63557eb878c
derv82 <derv82@gmail.com>
2016-04-06 21:29:00 -0700

    refactoring some more. Supports a subset of Control frames.

--- CapFile.js
+++ CapFile.js
@@ -13,8 +13,13 @@ function CapFile(bytes, debug) {
 
     this.bytes_ = bytes;
     this.byteOffset_ = 0;
+    this.bytesTotal = this.bytes_.length;
+    this.bytesHex = this.getHex(0, this.bytesTotal, " ");
 
     this.parse();
+
+    delete this.bytes_;
+    delete this.byteOffset_;
 };
 
 // Configurations
@@ -64,36 +69,36 @@ CapFile.prototype.getInt = function(startIndex, endIndex, useBigEndian, signed) 
     startIndex += this.byteOffset_;
     endIndex += this.byteOffset_;
 
-    var result = 0, i, x;
+    var intResult = 0, i, x;
     if (useBigEndian == true || CapFile.useBigEndian) {
         for (i = startIndex; i < endIndex; i++) {
-            result = result << 8;
+            intResult = intResult << 8;
             x = this.bytes_.charCodeAt(i);
-            result = result | x;
+            intResult = intResult | x;
         }
     } else {
         for (i = endIndex - 1; i >= startIndex; i--) {
-            result = result << 8;
+            intResult = intResult << 8;
             x = this.bytes_.charCodeAt(i);
-            result = result | x;
+            intResult = intResult | x;
         }
     }
 
     if (!signed) {
         // convert to unsigned.
         // See http://stackoverflow.com/a/1908655
-        result = (result >>> 0);
+        intResult = (intResult >>> 0);
     }
-    return result;
+    return intResult;
 };
 
 CapFile.prototype.getHex = function(startIndex, endIndex, byteSpacer, colSpacer, rowSpacer) {
     startIndex += this.byteOffset_;
     endIndex += this.byteOffset_;
 
-    var result = [], hex, i;
+    var byteList = [], hex, i;
     if (byteSpacer) {
-        result.push("");
+        byteList.push("");
     }
     for (i = startIndex; i < endIndex; i++) {
         hex = this.bytes_.charCodeAt(i).toString(16);
@@ -105,12 +110,27 @@ CapFile.prototype.getHex = function(startIndex, endIndex, byteSpacer, colSpacer,
         } else if (colSpacer && (i + 1) % 8 == 0) {
             hex += colSpacer;
         }
-        result.push(hex);
+        byteList.push(hex);
     }
     if (CapFile.useBigEndian) {
-        result.reverse();
+        byteList.reverse();
     }
-    return result.join(byteSpacer || "");
+    return byteList.join(byteSpacer || "");
+};
+
+CapFile.prototype.getBytes = function(startIndex, endIndex) {
+    startIndex += this.byteOffset_;
+    endIndex += this.byteOffset_;
+
+    var byteList = [], hex, i;
+    for (i = startIndex; i < endIndex; i++) {
+        hex = this.bytes_.charAt(i);
+        byteList.push(hex);
+    }
+    if (CapFile.useBigEndian) {
+        byteList.reverse();
+    }
+    return byteList.join("");
 };
 
 /**
@@ -134,7 +154,7 @@ CapFile.GlobalHeader = function() {
         CapFile.useBigEndian = true;
     }
 
-    var result = {
+    var headers = {
 
         // Version of cap file.
         version: this.getInt(4, 6).toString(10) + "." + this.getInt(6, 8).toString(10),
@@ -159,35 +179,44 @@ CapFile.GlobalHeader = function() {
         console.log("[CapFile.js, Debug] GlobalHeader (24 bytes):\n" + this.getHex(0, 24, " ", "  ", "\n"));
     }
 
-    return result;
+    return headers;
 };
 
 CapFile.WlanFrame = function() {
-    var result = {};
+    var frame = {};
 
     // Parse header
     // Details on https://wiki.wireshark.org/Development/LibpcapFileFormat
-    result.header = {
+    frame.header = {
         timestampSec: this.getInt(0, 4),
         timestampMicrosec: this.getInt(4, 8),
         length: this.getInt(8, 12),
         originalLength: this.getInt(12, 16),
     };
 
+    // Convert timestamps to date.
+    var ts_usec = frame.header.timestampSec * 1000;
+    ts_usec += (frame.header.timestampMicrosec / 1000);
+    ts_usec += this.globalHeader.gmtOffset;
+    frame.header.timestamp = new Date(ts_usec);
+
     // Shift to frame body.
     this.byteOffset_ += CapFile.PACKET_HEADER_LENGTH;
+
+    // Mark where this packet ends.
+    var endOfPacketOffset = this.byteOffset_ + frame.header.originalLength;
 
     // Parse fields that are present in all Wlan frames.
     // Details on https://en.wikipedia.org/wiki/IEEE_802.11#Layer_2_.E2.80.93_Datagrams
     var frameControlBits = this.getInt(0, 1);
-    result.frameControl = {
+    frame.frameControl = {
         version:  (frameControlBits >>> 0) & 0b11,
         type:     (frameControlBits >>> 2) & 0b11,
         subtype:  (frameControlBits >>> 4) & 0b1111,
     };
 
     var frameControlFlags = this.getInt(1, 2);
-    result.frameControl.flags = {
+    frame.frameControl.flags = {
         toDS:          !!(frameControlFlags >>> 0 & 0b1),
         fromDS:        !!(frameControlFlags >>> 1 & 0b1),
         moreFragments: !!(frameControlFlags >>> 2 & 0b1),
@@ -198,40 +227,114 @@ CapFile.WlanFrame = function() {
         order:         !!(frameControlFlags >>> 7 & 0b1),
     };
 
-    result.duration = this.getInt(2, 4);
+    frame.duration = this.getInt(2, 4);
 
-    result.addr1 = this.getHex(4, 10);
+    frame.addr1 = this.getHex(4, 10);
 
-    // Parse body based on type/subtype
-    var typeSubtype = result.frameControl.type + "." + result.frameControl.subtype;
-    if (CapFile.FrameTypes.hasOwnProperty(typeSubtype)) {
-        // We support this subtype
-        console.log("OY", CapFile.FrameTypes[typeSubtype]);
-        CapFile.FrameTypes[typeSubtype].builder.call(this, result);
+    // From here on, the fields may vary depending on the Frame Type (MANAGEMENT, CONTROL, DATA).
+    if (frame.frameControl.type === CapFile.WlanFrame.Types.MANAGEMENT) {
+        // Non-802.11 parameters are easy.
+        frame.addr2 = this.getHex(10, 16);
+        frame.addr3 = this.getHex(16, 22);
+        var frag_seq = this.getInt(22, 24);
+        frame.fragment_number = (frag_seq >>> 0) & 0b1111;
+        frame.sequence_number = (frag_seq >>> 4) & 0b111111111111;
+
+        // Skip to 802.1x frame
+        this.byteOffset_ += 24;
+
+        // Parse frame in context of a Control Frame.
+        CapFile.WlanFrame.Control.call(this, frame, endOfPacketOffset);
+    }
+    else if (frame.frameControl.type === CapFile.WlanFrame.Types.CONTROL) {
+        // No other data to parse.
+    }
+    else if (frame.frameControl.type === CapFile.WlanFrame.Types.DATA) {
+        // Parse data depending on subtype.
     }
 
     // Shift to end of frame
-    this.byteOffset_ += result.header.originalLength;
+    this.byteOffset_ = endOfPacketOffset;
 
-    return result;
+    return frame;
 };
 
-
-CapFile.WlanFrame_Beacon = function(result) {
-    
+CapFile.WlanFrame.Types = {
+    MANAGEMENT: 0,
+    CONTROL: 1,
+    DATA: 2
 };
 
-CapFile.FrameTypes = {
-    /*
-    "0.0": {
-        name: "AssociationRequest",
-    },
-    "0.1": {
-        name: "AssociationResponse",
-    },
-    */
-    "0.8": {
-        name: "Beacon",
-        builder: CapFile.WlanFrame_Beacon
-    },
+CapFile.WlanFrame.Control = function(frame, endOfPacketOffset) {
+    // TODO: Parse fixed and (if they exist) tagged parameters.
+    //       Fixed params length varies depending on subtype.
+    //       Beacon:12, Deauth:2, ProbeResponse:12, AssociationResponse:6,
+    var fixedParamLength;
+    if (frame.frameControl.subtype === 0) {
+        frame.name = "AssociationRequest";
+        fixedParamLength = 4;
+    }
+    else if (frame.frameControl.subtype === 1) {
+        frame.name = "AssociationResponse";
+        fixedParamLength = 6;
+    }
+    else if (frame.frameControl.subtype === 8) {
+        frame.name = "Beacon";
+        fixedParamLength = 12;
+    }
+    else if (frame.frameControl.subtype === 5) {
+        frame.name = "ProbeResponse";
+        fixedParamLength = 12;
+    }
+    else if (frame.frameControl.subtype === 11) {
+        frame.name = "Authentication";
+        fixedParamLength = 6;
+    }
+    else if (frame.frameControl.subtype === 12) {
+        frame.name = "Deauthentication";
+        fixedParamLength = 2;
+    }
+    else if (frame.frameControl.subtype === 13) {
+        frame.name = "Action";
+        fixedParamLength = 9;
+    }
+
+    console.log("Fixed param length: " + fixedParamLength);
+    if (!fixedParamLength) {
+        frame.name = "Unknown";
+        // Unable to parse tagged parameters without knowing fixed parameter length.
+        return;
+    }
+
+    // TODO: Parse fixed parameters.
+
+    this.byteOffset_ += fixedParamLength;
+
+    frame.taggedParameters = {};
+
+    // Parse tagged parameters.
+    while (this.byteOffset_ < endOfPacketOffset) {
+        var tag = {};
+        var tagIndex = this.getInt(0, 1);
+        var tagLength = this.getInt(1, 2);
+        if (tagIndex === 0) {
+            // SSID
+            var tagData = this.getBytes(2, 2 + tagLength);
+            frame.taggedParameters[tagIndex] = {
+                name: "SSID",
+                length: tagLength,
+                data: tagData
+            };
+        }
+        else {
+            var tagData = this.getHex(2, 2 + tagLength);
+            frame.taggedParameters[tagIndex] = {
+                name: "Unknown",
+                length: tagLength,
+                data: tagData
+            };
+        }
+        this.byteOffset_ += tagLength + 2;
+    }
 };
+
