2f83dc08a830a40ab56d070944bee42ca117f9b2
derv82 <derv82@gmail.com>
2016-04-07 01:17:22 -0700

    Bug fixes, removing unused Wpa.js (already beyond feature parity in CapFile.js)

--- CapFile.js
+++ CapFile.js
@@ -1,12 +1,53 @@
 /*
+ * DESIGN
  * Using string-as-bytes per https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Sending_and_Receiving_Binary_Data
  */
 
 /**
+ * RESEARCH
+ * Additional Reading and References:
+ *
+ * Details on WPA encryption (byte-level) - http://sid.rstack.org/pres/0810_BACon_WPA2_en.pdf
+ *  - Includes details on "caching" repeated calls to "BODY"
+ *
+ * HMAC-SHA1 in JS - https://github.com/Caligatio/jsSHA/tree/v2.0.1
+ *  - Try online: https://caligatio.github.io/jsSHA/
+ *  - Text: "value"
+ *  - Key: "secret"
+ * 
+ * PBKDF2:
+ *  - Apparently it's HMAC-SHA1() * 8192
+ *  - But see https://en.wikipedia.org/wiki/PBKDF2
+ *  - PBKDF2, DK = PBKDF2(PRF,       Password,   Salt, count, dkLen)
+ *  - WPA,    DK = PBKDF2(HMAC−SHA1, passphrase, ssid,  4096, 256)
+ *
+ *
+ * Load byte array from URL: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Sending_and_Receiving_Binary_Data
+ *
+ * aircrack-ng sample WPA file: http://www.aircrack-ng.org/doku.php?id=wpa_capture
+ * Parsing .cap files: http://systemsarchitect.net/2014/03/12/parsing-binary-data-in-php-on-an-example-with-the-pcap-format/
+ *
+ * http://www.willhackforsushi.com/papers/80211_Pocket_Reference_Guide.pdf
+ *
+ * Details on 802.11 packet structures - http://www.studioreti.it/slide/802-11-Frame_E_C.pdf
+ *
+ *
+ * TODO: 
+ *  - Check bytes_.length >= header.length before parsing! (chocobo-new.cap is 'truncated', gives weird results)
+ *  - Look at using ArrayBuffer + DataView internal JS structures.
+ *  - Browser compatibility  (ie, chrome, safari)
+ */
+
+
+/**
  * Parse the given bytes of a Packet Capture (PCAP) file.
- * Loads result into this.globalHeader and this.packetFrames.
+ * Loads result into boejct this.globalHeader (always) and list this.packetFrames (for known frame types).
+ *
+ * @param bytes - (string? bytes?) Raw bytes from a .cap Pcap file.
+ * @param debug - (boolean) Flag to dump debug information to the console (default: false)
+ *
  * TODO: Support 'debug' as a *callback*, sends debug text. e.g.
- *       var cf = CapFile(bytes, function(txt) { div.text = txt });
+ *       var cf = CapFile(bytes, function(txt) { input.value += txt + "\n" });
  */
 function CapFile(bytes, debug) {
     CapFile.debug = debug || false;
@@ -37,9 +78,10 @@ CapFile.PACKET_HEADER_LENGTH = 16;
  */
 CapFile.prototype.parse = function() {
     // First chunk of bytes is global header.
+    this.byteOffset_ = 0;
     this.globalHeader = CapFile.GlobalHeader.call(this);
 
-    // Ensure we can parse this data.
+    // Ensure we can parse this Pcap file/ format version.
     if (this.globalHeader.version !== CapFile.SUPPORTED_PCAP_VERSION) {
         throw Error("Unsupported PCap File version (" + this.globalHeader.version + "). " +
                 "Unable to parse.");
@@ -51,19 +93,32 @@ CapFile.prototype.parse = function() {
                 "Unable to parse.");
     }
 
+    // Skip past Blobal Header bytes.
+    this.byteOffset_ += CapFile.GLOBAL_HEADER_LENGTH;
+
     // List of all identified frames in the cap file.
     this.packetFrames = [];
 
     var frame;
-
-    this.byteOffset_ += CapFile.GLOBAL_HEADER_LENGTH;
     while (this.byteOffset_ < this.bytes_.length) {
         frame = CapFile.WlanFrame.call(this);
-
-        this.packetFrames.push(frame);
+        if (frame.name.indexOf("Unknown") === -1) {
+            // Only add known packet types to frames list.
+            this.packetFrames.push(frame);
+        }
     }
 };
 
+/**
+ * Extract integer from bytes_ at current byteOffset_
+ *
+ * @param startIndex   - Index of first byte (added to byteOffset_)
+ * @param endIndex     - Index of last byte (added to byteOffset_)
+ * @param useBigEndian - Override configuration, expect big-endian-style byte order (default: CapFile.useBigEndian)
+ * @param signed       - If integer should be signed (default: false)
+ *
+ * @return (int) Numeric-representation of data at byte location.
+ */
 CapFile.prototype.getInt = function(startIndex, endIndex, useBigEndian, signed) {
     startIndex += this.byteOffset_;
     endIndex += this.byteOffset_;
@@ -91,6 +146,17 @@ CapFile.prototype.getInt = function(startIndex, endIndex, useBigEndian, signed) 
     return intResult;
 };
 
+/**
+ * Extract hex characters from bytes_ at current byteOffset_
+ *
+ * @param startIndex - (int) Index of first byte (added to byteOffset_)
+ * @param endIndex   - (int) Index of last byte (added to byteOffset_)
+ * @param byteSpacer - (string) Separator between bytes (default: empty string)
+ * @param colSpacer  - (string) Separator between chunks of 8 bytes (default: empty string)
+ * @param rowSpacer  - (string) Separator between chunks of 16 bytes (default: empty string)
+ *
+ * @return (string) Hex-representation of data at byte location.
+ */
 CapFile.prototype.getHex = function(startIndex, endIndex, byteSpacer, colSpacer, rowSpacer) {
     startIndex += this.byteOffset_;
     endIndex += this.byteOffset_;
@@ -118,6 +184,14 @@ CapFile.prototype.getHex = function(startIndex, endIndex, byteSpacer, colSpacer,
     return byteList.join(byteSpacer || "");
 };
 
+/**
+ * Extract raw bytes from bytes_ at current byteOffset_
+ *
+ * @param startIndex - (int) Index of first byte (added to byteOffset_)
+ * @param endIndex   - (int) Index of last byte (added to byteOffset_)
+ *
+ * @return (string? bytes?) Raw bytes of data at byte location.
+ */
 CapFile.prototype.getBytes = function(startIndex, endIndex) {
     startIndex += this.byteOffset_;
     endIndex += this.byteOffset_;
@@ -134,7 +208,18 @@ CapFile.prototype.getBytes = function(startIndex, endIndex) {
 };
 
 /**
- * Details on https://wiki.wireshark.org/Development/LibpcapFileFormat
+ * Parses Pcap file global header (starting at byteOffset_, presumably "0").
+ *
+ * Requires reference to "this" CapFile object.
+ *
+ * More info on https://wiki.wireshark.org/Development/LibpcapFileFormat
+ *
+ * @return (object) containing:
+ *     version        - (string) in format <major>.<minor> e.g. 2.4
+ *     gmtOffset      - (signed int) Offset between packet timestamps and GMT timezone
+ *     sigFigs        - (int) Accuracy of timestamps
+ *     snapshotLength - (int) Length of snapshot for the capture (in bytes)
+ *     headerType     - (int) Link-Layer header type, e.g. LINKTYPE_IEEE802.11 = 105 (Wireless LAN)
  */
 CapFile.GlobalHeader = function() {
     // Presume big endian.
@@ -182,23 +267,66 @@ CapFile.GlobalHeader = function() {
     return headers;
 };
 
+/**
+ * "Frame Builder". Parses entire frame at current bytesOffset_ location.
+ * Only supports detailed parsing of certain 802.11 WLAN frames - see CapFile.WlanFrame.* for more info
+ *
+ * Requires reference to "this" CapFile object -- call using CapFile.WlanFrame.call(this);
+ *
+ * @return (object) Frame information containing: {
+ *      header: {
+ *          timestamp: (Date) Timestamp of the packet as a Date object
+ *          length:    (int)  Length of the packet/frame (in bytes)
+ *      },
+ *      frameControl: {
+ *          version: (int) Frame version
+ *          type:    (int) Frame type (0=Management, 1=Control, 2=Data)
+ *          subtype: (int) Frame subtype
+ *          flags: {
+ *              toDS:         (boolean)
+ *              fromDS:       (boolean)
+ *              moreFragments:(boolean)
+ *              retry:        (boolean)
+ *              powerMgt:     (boolean)
+ *              moreData:     (boolean)
+ *              is_protected: (boolean)
+ *              order:        (boolean)
+ *          }
+ *      },
+ *      duration: (int)
+ *      addr1:    (string) First address, as hex characters (no separator).
+ *  }
+ *
+ *  CONTROL FRAMES (type:1) will not contain any additional information.
+ *
+ *  MANAGEMENT FRAMES (type:0) and DATA FRAMES (type:2) both contain more information: {
+ *      addr2: (string) Second address, as hex characters (no separator).
+ *      addr3: (string) Second address, as hex characters (no separator).
+ *      fragmentNumber: (int)
+ *      sequenceNumber: (int)
+ *  }
+ *
+ * More info on MANAGEMENT frames: see CapFile.WlanFrame.Management
+ * More info on DATA frames:       see CapFile.WlanFrame.Data
+ *
+ */
 CapFile.WlanFrame = function() {
     var frame = {};
 
     // Parse header
     // Details on https://wiki.wireshark.org/Development/LibpcapFileFormat
+
+    // Convert timestamp to Date.
+    var timestampSec = this.getInt(0, 4);
+    var timestampUsec = this.getInt(4, 8);
+    var ts_usec = timestampSec * 1000;
+    ts_usec += (timestampUsec / 1000);
+    ts_usec += this.globalHeader.gmtOffset;
     frame.header = {
-        timestampSec: this.getInt(0, 4),
-        timestampMicrosec: this.getInt(4, 8),
+        timestamp: new Date(ts_usec),
         length: this.getInt(8, 12),
         originalLength: this.getInt(12, 16)
     };
-
-    // Convert timestamps to date.
-    var ts_usec = frame.header.timestampSec * 1000;
-    ts_usec += (frame.header.timestampMicrosec / 1000);
-    ts_usec += this.globalHeader.gmtOffset;
-    frame.header.timestamp = new Date(ts_usec);
 
     // Shift to frame body.
     this.byteOffset_ += CapFile.PACKET_HEADER_LENGTH;
@@ -238,9 +366,9 @@ CapFile.WlanFrame = function() {
         // Capture similarities between Management and Data frames.
         frame.addr2 = this.getHex(10, 16);
         frame.addr3 = this.getHex(16, 22);
-        var frag_seq = this.getInt(22, 24);
-        frame.fragment_number = (frag_seq >>> 0) & 0b1111;
-        frame.sequence_number = (frag_seq >>> 4) & 0b111111111111;
+        var fragSeq = this.getInt(22, 24);
+        frame.fragmentNumber = (fragSeq >>> 0) & 0b1111;
+        frame.sequenceNumber = (fragSeq >>> 4) & 0b111111111111;
 
         // Skip to just past the sequence number.
         this.byteOffset_ += 24;
@@ -255,7 +383,7 @@ CapFile.WlanFrame = function() {
         }
     }
 
-    // Shift to end of frame
+    // Shift to end of frame.
     this.byteOffset_ = endOfPacketOffset;
 
     return frame;
@@ -267,10 +395,25 @@ CapFile.WlanFrame.Types = {
     DATA: 2
 };
 
+/**
+ * Adds any additional information about this Management packet.
+ * Only a subset of Management frames are supported.
+ *
+ * Focus is on getting SSID from the Management frames' "tagged parameters".
+ * Other tagged parameters are read (and stored as Hex) but are not parsed.
+ *
+ * Requires reference to "this" CapFile object -- call using CapFile.WlanFrame.Management.call(this, ...);
+ *
+ * Increments CapFile.byteOffset_ to end of the Management frame.
+ *
+ * @param frame - (object) Reference to the currently-parsed frame.
+ * @param endOfPacketOffset - (int) The Offset, in relation to byteOffset_, in which this packet ends.
+ */
 CapFile.WlanFrame.Management = function(frame, endOfPacketOffset) {
-    // TODO: Parse fixed and (if they exist) tagged parameters.
-    //       Fixed params length varies depending on subtype.
-    //       Beacon:12, Deauth:2, ProbeResponse:12, AssociationResponse:6,
+    // Management frames contain:
+    // 1. Fixed Parameters (variable length, depends on frameControl.subtype).
+    // 2. Tagged Parameters (variable length, defined in 'length' bytes).
+
     var fixedParamLength;
     if (frame.frameControl.subtype === 0) {
         frame.name = "AssociationRequest";
@@ -302,18 +445,17 @@ CapFile.WlanFrame.Management = function(frame, endOfPacketOffset) {
     }
 
     if (!fixedParamLength) {
-        frame.name = "Unknown";
         // Unable to parse tagged parameters without knowing fixed parameter length.
+        frame.name = "Unknown Management Frame subtype (" + frame.frameControl.subtype + ")";
         return;
     }
 
-    // TODO: Parse fixed parameters.
-
+    // TODO: Parse fixed parameters. Skipping for now.
     this.byteOffset_ += fixedParamLength;
 
-    frame.taggedParameters = {};
 
     // Parse tagged parameters.
+    frame.taggedParameters = {};
     while (this.byteOffset_ < endOfPacketOffset) {
         var tag = {};
         var tagIndex = this.getInt(0, 1);
@@ -328,18 +470,39 @@ CapFile.WlanFrame.Management = function(frame, endOfPacketOffset) {
             };
         }
         else {
+            // Don't care about other tags.
             var tagData = this.getHex(2, 2 + tagLength);
             frame.taggedParameters[tagIndex] = {
-                name: "Unknown",
+                name: "N/A",
                 length: tagLength,
                 data: tagData
             };
         }
+        // Shift to next tagged paramter (or end of packet).
         this.byteOffset_ += tagLength + 2;
     }
 };
 
+/**
+ * Adds any additional information about this Data frame.
+ * Only a small subset of Data frames are supported.
+ *
+ * Focus is on getting EAPOL (WPA handshake-related) information.
+ *
+ * Requires reference to "this" CapFile object -- call using CapFile.WlanFrame.Data.call(this, ...);
+ *
+ * Increments CapFile.byteOffset_ to end of the Data frame (if known).
+ * Otherwise does not change CapFile.byteOffset_
+ *
+ * @param frame - (object) Reference to the currently-parsed frame.
+ * @param endOfPacketOffset - (int) The Offset, in relation to byteOffset_, in which this packet ends.
+ */
 CapFile.WlanFrame.Data = function(frame, endOfPacketOffset) {
+    if ((frame.frameControl.subtype & 0b111) !== 0) {
+        // Only support EAPOL (and EAPOL+QoS) packets.
+        frame.name = "Unknown Data Frame subtype (" + frame.frameControl.subtype + ")";
+        return;
+    }
     if (frame.frameControl.flags.toDS && frame.frameControl.flags.fromDS) {
         // toDS and fromDS are set, expect addr4
         frame.addr4 = this.getHex(0, 6);
@@ -347,11 +510,15 @@ CapFile.WlanFrame.Data = function(frame, endOfPacketOffset) {
     }
     if ((frame.frameControl.subtype & 0b1000) === 8) {
         // QoS flag is set. Expect QoS control field.
-        this.qosControl = this.getHex(0, 2);
+        frame.qosControl = this.getHex(0, 2);
         this.byteOffset_ += 2;
+        frame.name = "EAPOL (QoS)";
+    }
+    else {
+        frame.name = "EAPOL";
     }
     if (frame.frameControl.flags.order) {
-        // Expect HT Control field.
+        // Expect (and skip) HT Control field.
         this.byteOffset_ += 4;
     }
 
@@ -375,10 +542,10 @@ CapFile.WlanFrame.Data = function(frame, endOfPacketOffset) {
         keyID:              this.getHex(73, 81),
         keyMIC:             this.getHex(81, 97),
         keyDataLength:      this.getInt(97, 99, true, false)
-    }
-    frame.auth.keyInformation = {
+    };
+    frame.auth.keyInfoFlags = {
         keyDescriptorVersion:      (frame.auth.keyInfo >>>  0) & 0b111, // 2=AES Cipher, HMAC-SHA1 MIC
-        keyType:                   (frame.auth.keyInfo >>>  3) & 0b1,   // 1=pairwise key
+        keyType:                   (frame.auth.keyInfo >>>  3) & 0b1,   // 1=Pairwise Key
         keyIndex:                  (frame.auth.keyInfo >>>  4) & 0b11,
         install:                !!((frame.auth.keyInfo >>>  6) & 0b1 ),
         ack:                    !!((frame.auth.keyInfo >>>  7) & 0b1 ),
@@ -391,9 +558,53 @@ CapFile.WlanFrame.Data = function(frame, endOfPacketOffset) {
     this.byteOffset_ += 99;
 
     if (frame.auth.keyDataLength > 0) {
-        frame.auth.keyData = this.getHex(99, 99 + frame.auth.keyDataLength);
+        frame.auth.keyData = this.getHex(0, frame.auth.keyDataLength);
         this.byteOffset_ += frame.auth.keyDataLength;
     }
 
+};
+
+/**
+ * TODO: Identify 4-way handshake(s), extract information required for calculating PMK.
+ */
+CapFile.prototype.extractPmkFields = function() {
+    var ssid, snonce, anonce, replay_counter, mic, frame_bytes;
+    // Look for SSID name in previous beacons/auth packets
+
+    // Look for last 3 frames of handshake
+
+    /* Handshake (2 of 4):
+     * mic:true
+     * ack:false
+     * install:false
+     * keyDataLength > 0)
+     * 
+     * Extract:
+     * - keynonce (SNonce , the nonce from STATION)
+     */
+
+    /* Handshake (3 of 4):
+     * mic:true
+     * ack:true
+     * install:true
+     * 
+     * Extract:
+     * - src_address (STATION)
+     * - dst_address (AP)
+     * - ANonce (from AP)
+     * - replay_counter (for Handshake 4 of 4)
+     */
+
+    /* Handshake (4 of 4):
+     * mic:true
+     * ack:false
+     * install:false
+     * replay_couner: <same as Handshake (3 of 4)>
+     * (And/Or) key_data_length == 0 (data === undefined)
+     * 
+     * Extract:
+     * - MIC
+     * - "EAPOL frame"
+     */
 };
 
